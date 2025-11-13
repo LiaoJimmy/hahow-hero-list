51cbd1ec0bcbc45c037a27501f1aaba3f3a31bb0
Jimmy Liao <gooyumi@gmail.com>
2025-11-13 15:37:42 +0800

    Open index and redirect to hero page

--- app/routes.ts
+++ app/routes.ts
@@ -2,10 +2,12 @@ import type { RouteConfig } from '@react-router/dev/routes';
 import { index, layout, prefix, route } from '@react-router/dev/routes';
 
 export default [
+  index('./routes/Home.tsx'),
   ...prefix('heroes', [
     layout('./routes/HeroList/HeroListLayout.tsx', [
       index('./routes/HeroList/HeroListIndex.tsx'),
       route(':heroId', './routes/HeroProfile/HeroProfile.tsx'),
     ]),
   ]),
+  route('*', './routes/CatchAll.tsx'),
 ] satisfies RouteConfig;
